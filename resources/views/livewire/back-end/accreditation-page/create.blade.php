96bcf50ce70833f5ed9d14d5294aa9a9973d2bc4
sulthanalif <sulthanalif45@gmail.com>
2025-05-16 21:29:14 +0700

    Restricts accreditation file uploads to images
    Updates the file upload validation and input field to only accept image files (jpeg, png, jpg) for accreditation receipts. This ensures that only valid image files are uploaded, improving data integrity.

--- resources/views/livewire/back-end/accreditation-page/create.blade.php
+++ resources/views/livewire/back-end/accreditation-page/create.blade.php
@@ -14,7 +14,7 @@
         </div>
 
         <div class="flex justify-between items-center">
-            <x-file wire:model="file" label="Receipt" hint="Only PDF/Image" accept="application/pdf, image/png, image/jpeg, image/jpg" />
+            <x-file wire:model="file" label="Receipt" hint="Only Image" accept="image/png, image/jpeg, image/jpg" />
             @if ($this->recordId)
                 <x-button label="Download File" @click="$wire.downloadFile" icon="o-arrow-down" class="btn-primary" spinner="downloadFile" />
             @endif
