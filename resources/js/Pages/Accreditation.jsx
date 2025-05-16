ba3eb6d7f26b6e58f7098714a553bde79928ce97
Muh Hilman Sholehudin <muhhilmansholehudin@gmail.com>
2025-05-16 21:46:56 +0700

    <update> [Frontend]: Consume Data on Page Accreditation

--- resources/js/Pages/Accreditation.jsx
+++ resources/js/Pages/Accreditation.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../Components/Layout";
 import { Link } from "@inertiajs/react";
+import useApi from "../Hooks/response";
 
 const Accreditation = () => {
+    const [activeTab, setActiveTab] = useState(null);
+
     useEffect(() => {
         const link1 = document.createElement("link");
         link1.rel = "stylesheet";
@@ -30,169 +33,185 @@ const Accreditation = () => {
         };
     }, []);
 
-    return (
-        <>
-            <Layout>
-                <div className="home">
-                    <div className="breadcrumbs_container">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col">
-                                    <div className="breadcrumbs">
-                                        <ul>
-                                            <li>
-                                                <Link href="/">Home</Link>
-                                            </li>
-                                            <li>Curriculum</li>
-                                            <li>Accreditation</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    const {
+        data: accreditations,
+        loading,
+        error,
+        get: getAccreditations,
+    } = useApi("accreditations");
 
-                <div className="feature">
-                    <div
-                        className="feature_background"
-                        style={{
-                            backgroundImage:
-                                "url(/landing/images/courses_background.jpg)",
-                        }}
-                    />
+    useEffect(() => {
+        if (accreditations?.accreditations?.length > 0 && activeTab === null) {
+            setActiveTab(accreditations.accreditations[0].id);
+        }
+    }, [accreditations, activeTab]);
+
+    const handleRefresh = () => {
+        getAccreditations();
+    };
+
+    return (
+        <Layout>
+            <div className="home">
+                <div className="breadcrumbs_container">
                     <div className="container">
                         <div className="row">
                             <div className="col">
-                                <div className="section_title_container text-center">
-                                    <h2 className="section_title">
-                                        Our Accreditation Excellence
-                                    </h2>
-                                    <div className="section_subtitle">
-                                        <p>
-                                            Validating our commitment to quality
-                                            education through recognized
-                                            accreditation
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row feature_row">
-                            {/* Feature Content */}
-                            <div className="col-lg-3 feature_col">
-                                <div className="feature_content">
-                                    <div
-                                        class="list-group"
-                                        id="list-tab"
-                                        role="tablist"
-                                    >
-                                        <a
-                                            class="list-group-item list-group-item-action active"
-                                            id="list-sma-list"
-                                            data-toggle="list"
-                                            href="#list-sma"
-                                            role="tab"
-                                            aria-controls="sma"
-                                        >
-                                            Senior High School
-                                        </a>
-                                        <a
-                                            class="list-group-item list-group-item-action"
-                                            id="list-smp-list"
-                                            data-toggle="list"
-                                            href="#list-smp"
-                                            role="tab"
-                                            aria-controls="smp"
-                                        >
-                                            Junior High School
-                                        </a>
-                                        <a
-                                            class="list-group-item list-group-item-action"
-                                            id="list-sd-list"
-                                            data-toggle="list"
-                                            href="#list-sd"
-                                            role="tab"
-                                            aria-controls="sd"
-                                        >
-                                            Elementary School
-                                        </a>
-                                        <a
-                                            class="list-group-item list-group-item-action"
-                                            id="list-edexcel-list"
-                                            data-toggle="list"
-                                            href="#list-edexcel"
-                                            role="tab"
-                                            aria-controls="edexcel"
-                                        >
-                                            Edexcel Certificate
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* Feature Video */}
-                            <div className="col-lg-9 feature_col">
-                                <div className="feature_content">
-                                    <div
-                                        class="tab-content"
-                                        id="nav-tabContent"
-                                    >
-                                        <div
-                                            class="tab-pane fade show active"
-                                            id="list-sma"
-                                            role="tabpanel"
-                                            aria-labelledby="list-sma-list"
-                                        >
-                                            <img
-                                                src="/landing/images/accreditation/akre-sma.jpg"
-                                                className="img-fluid"
-                                                alt="Accreditation SMA"
-                                            />
-                                        </div>
-                                        <div
-                                            class="tab-pane fade"
-                                            id="list-smp"
-                                            role="tabpanel"
-                                            aria-labelledby="list-smp-list"
-                                        >
-                                            <img
-                                                src="/landing/images/accreditation/akre-smp.jpg"
-                                                className="img-fluid"
-                                                alt="Accreditation SMP"
-                                            />
-                                        </div>
-                                        <div
-                                            class="tab-pane fade"
-                                            id="list-sd"
-                                            role="tabpanel"
-                                            aria-labelledby="list-sd-list"
-                                        >
-                                            <img
-                                                src="/landing/images/accreditation/akre-sd.jpg"
-                                                className="img-fluid"
-                                                alt="Accreditation SD"
-                                            />
-                                        </div>
-                                        <div
-                                            class="tab-pane fade"
-                                            id="list-edexcel"
-                                            role="tabpanel"
-                                            aria-labelledby="list-edexcel-list"
-                                        >
-                                            <img
-                                                src="/landing/images/accreditation/sertif-edexcel.jpg"
-                                                className="img-fluid"
-                                                alt="Sertifikat Edexcel"
-                                            />
-                                        </div>
-                                    </div>
+                                <div className="breadcrumbs">
+                                    <ul>
+                                        <li>
+                                            <Link href="/">Home</Link>
+                                        </li>
+                                        <li>Curriculum</li>
+                                        <li>Accreditation</li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </Layout>
-        </>
+            </div>
+
+            <div className="feature">
+                <div
+                    className="feature_background"
+                    style={{
+                        backgroundImage:
+                            "url(/landing/images/courses_background.jpg)",
+                    }}
+                />
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <div className="section_title_container text-center">
+                                <h2 className="section_title">
+                                    Our Accreditation Excellence
+                                </h2>
+                                <div className="section_subtitle">
+                                    <p>
+                                        Validating our commitment to quality
+                                        education through recognized
+                                        accreditation
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row feature_row">
+                        {loading ? (
+                            <div className="col-12 text-center py-5">
+                                <div
+                                    className="spinner-border text-primary"
+                                    role="status"
+                                >
+                                    <span className="sr-only">Loading...</span>
+                                </div>
+                            </div>
+                        ) : error ? (
+                            <div className="col-12 text-center py-5">
+                                <div className="alert alert-danger">
+                                    {error}
+                                </div>
+                            </div>
+                        ) : accreditations &&
+                          accreditations.accreditations.length > 0 ? (
+                            <>
+                                <div className="col-lg-3 feature_col mt-lg-3">
+                                    <div className="feature_content">
+                                        <div
+                                            className="list-group"
+                                            id="list-tab"
+                                            role="tablist"
+                                        >
+                                            {accreditations.accreditations.map(
+                                                (accreditation) => (
+                                                    <a
+                                                        key={accreditation.id}
+                                                        className={`list-group-item list-group-item-action ${
+                                                            activeTab ===
+                                                            accreditation.id
+                                                                ? "active"
+                                                                : ""
+                                                        }`}
+                                                        id={`list-${accreditation.id}-list`}
+                                                        data-toggle="list"
+                                                        href={`#list-${accreditation.id}`}
+                                                        role="tab"
+                                                        aria-controls={
+                                                            accreditation.id
+                                                        }
+                                                        onClick={(e) => {
+                                                            e.preventDefault();
+                                                            setActiveTab(
+                                                                accreditation.id
+                                                            );
+                                                        }}
+                                                    >
+                                                        {accreditation.title}
+                                                    </a>
+                                                )
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-9 feature_col">
+                                    <div className="feature_content">
+                                        <div
+                                            className="tab-content"
+                                            id="nav-tabContent"
+                                        >
+                                            {accreditations.accreditations.map(
+                                                (accreditation) => (
+                                                    <div
+                                                        key={accreditation.id}
+                                                        className={`tab-pane fade ${
+                                                            activeTab ===
+                                                            accreditation.id
+                                                                ? "show active"
+                                                                : ""
+                                                        }`}
+                                                        id={`list-${accreditation.id}`}
+                                                        role="tabpanel"
+                                                        aria-labelledby={`list-${accreditation.id}-list`}
+                                                    >
+                                                        <div className="feature_note">
+                                                            <h3 className="card-title">
+                                                                {
+                                                                    accreditation.title
+                                                                }
+                                                            </h3>
+                                                            <p className="card-text">
+                                                                {
+                                                                    accreditation.description
+                                                                }
+                                                            </p>
+                                                            <img
+                                                                src={`/storage/${accreditation.file}`}
+                                                                className="img-fluid"
+                                                                alt={
+                                                                    accreditation.title
+                                                                }
+                                                            />
+                                                        </div>
+                                                    </div>
+                                                )
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        ) : (
+                            <div className="col-12 text-center py-5">
+                                <div className="alert alert-secondary font-weight-bold h3">
+                                    No accreditation data available
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </Layout>
     );
 };
 
